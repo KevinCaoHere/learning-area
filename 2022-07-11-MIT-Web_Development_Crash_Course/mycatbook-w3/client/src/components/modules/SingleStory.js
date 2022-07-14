c72b1dd682d2ec7e0bfc6b3d168d0dc1c5841bb4
KevinCaoHere <KevinCaoHere@outlook.com>
2022-07-14 23:45:24 +0800

    w3-step2 & step3 complete

--- 2022-07-11-MIT-Web_Development_Crash_Course/mycatbook-w3/client/src/components/modules/SingleStory.js
+++ 2022-07-11-MIT-Web_Development_Crash_Course/mycatbook-w3/client/src/components/modules/SingleStory.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 // TODO (step2): import Card.css
+import "./Card.css";
 
 /**
  * Story is a component that renders creator and content of a story
@@ -10,11 +11,13 @@ import React from "react";
  * @param {string} creator_name
  * @param {string} content of the story
  */
-const SingleStory = () => {
+const SingleStory = (props) => {
   return (
     <div className="Card-story">
-      This is a SingleStory!
       {/* TODO (step2): use JSX and props to render story creator and content */}
+      <div className="Card-storyUser">{props.creator_name}</div>
+      <div className="Card-storyContent">{props.content}</div>
+      <hr/><br/>
     </div>
   );
 };
--- 2022-07-11-MIT-Web_Development_Crash_Course/mycatbook-w3/client/src/components/pages/Feed.js
+++ 2022-07-11-MIT-Web_Development_Crash_Course/mycatbook-w3/client/src/components/pages/Feed.js
@@ -3,6 +3,7 @@ import { get } from "../../utilities";
 // TODO (step2): import SingleStory
 // TODO (step4): import NewStory
 // TODO (step6): remove SingleStory import, import Card
+import SingleStory from "../modules/SingleStory"; //fxxk
 
 const Feed = () => {
   // TODO (step1): define state to hold stories
@@ -13,15 +14,29 @@ const Feed = () => {
     // and assign it to state
     get("/api/stories").then((props) => {
       setStories(props);
-    }); 
+    });
   }, []);
 
-  return <div>{JSON.stringify(stories)}</div>;
+  // TODO (step1): render the raw stories data from state
+  // return <div>{JSON.stringify(stories)}</div>;
   // Stories is an object. Stringfy is needed when printing the state.
 
-  // TODO (step1): render the raw stories data from state
   // TODO (step2): render a SingleStory with hardcoded props
+  // return (
+  //   <div>
+  //     <SingleStory creator_name="Kevin Cao" content="Hello, world!" />
+  //   </div>
+  // );
+
   // TODO (step3): map the state to SingleStory components
+  return stories.map((item) => {
+    return (
+      <div>
+        <SingleStory creator_name={item.creator_name} content={item.content} />
+      </div>
+    );
+  });
+
   // TODO (step4): add in the NewStory component
   // TODO (step6): use Card instead of SingleStory
 };
