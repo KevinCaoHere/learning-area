1ea87bb842746682cadfa3aeddf5e7e3ca680913
KevinCaoHere <KevinCaoHere@outlook.com>
2022-07-22 23:04:24 +0800

    w4 start!

---
+++ 2022-07-11-MIT-Web_Development_Crash_Course/mycatbook-w4/server/server.js
@@ -0,0 +1,25 @@
+/*
+|--------------------------------------------------------------------------
+| server.js -- The core of your server
+|--------------------------------------------------------------------------
+|
+| This file defines how your server starts up. Think of it as the main() of your server.
+| At a high level, this file does the following things:
+| - Connect to the database (TODO: WORKSHOP 5)
+| - Sets up server middleware (i.e. addons that enable things like json parsing) (TODO: WORKSHOP 3)
+| - Hooks up all the backend routes specified in api.js (TODO: WORKSHOP 4)
+| - Sets up error handling in case something goes wrong when handling a request (TODO: WORKSHOP 3)
+| - Actually starts the webserver
+*/
+
+// import libraries needed for the webserver to work!
+const express = require("express"); // backend framework for our node server.
+
+// create a new express server
+const app = express();
+
+// hardcode port to 3000 for now
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
