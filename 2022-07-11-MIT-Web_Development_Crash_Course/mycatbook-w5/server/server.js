9965def2f8ccddc5c264b5836dcb0ae8f1262764
KevinCaoHere <KevinCaoHere@outlook.com>
2022-07-26 21:51:09 +0800

    w5 start!

--- 2022-07-11-MIT-Web_Development_Crash_Course/mycatbook-w5/server/server.js
+++ 2022-07-11-MIT-Web_Development_Crash_Course/mycatbook-w5/server/server.js
@@ -14,37 +14,42 @@
 
 // import libraries needed for the webserver to work!
 const express = require("express"); // backend framework for our node server.
-const path = require("path");
+const path = require("path"); // provide utilities for working with file and directory paths
 
 // create a new express server
 const app = express();
 
-// internal server error handling
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-
-  if (status === 500) {
-    console.log("Error Occurred");
-    console.log(err);
-  }
-
-  res.status(status);
-  res.send({ status: status, message: err.message });
-});
-
-// back-end entrance
-const reactPath = path.resolve(__dirname, "..", "client", "dist"); // directory path
-app.use(express.static(reactPath));
+// allow us to make post requests
+app.use(express.json());
 
 app.get("/api/test", (req, res) => {
   res.send({ message: "Wow I made my first API!" });
 });
 
+// Load the compiled react files, which will serve /index.html and /bundle.js
+const reactPath = path.resolve(__dirname, "..", "client", "dist");
+app.use(express.static(reactPath));
+
+// for all other routes, render index.html and let the react router handle it
 app.get("*", (req, res) => {
-  // res.send("Endpoint B");
   res.sendFile(path.join(reactPath, "index.html"));
 });
 
+// any server errors cause this function to run
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  if( status === 500 ){
+    // 500 means Internal Server Error
+    console.log("The server errored when processing a request");
+    console.log(err);
+  }
+
+  res.status(status);
+  res.send({
+    status: status,
+    message: err.message,
+  });
+});
 
 // hardcode port to 3000 for now
 const port = 3000;
